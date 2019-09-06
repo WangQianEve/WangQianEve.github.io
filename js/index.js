cc1ab4242db946932835f431dee1765a6a3a1ee8
Qian <eli_home@outlook.com>
2019-09-06 12:20:12 -0400

    new layout of homepage and code pages

--- js/index.js
+++ js/index.js
@@ -53,38 +53,16 @@ function createFilter() {
     });
 }
 
-function turnOnDesign(initial = false) {
-    $('.title.design').addClass('active');
-    $('.title.code').removeClass('active');
-    const $design = $("#filter, .container.design");
-    const $code = $(".container.code");
-    fadeChange($code, $design, !initial);
-    if (!initial) window.location.hash = "#design";
-}
-
-function turnOnCode(initial = false) {
-    $('.title.design').removeClass('active');
-    $('.title.code').addClass('active');
-    const $design = $("#filter, .container.design");
-    const $code = $(".container.code");
-    fadeChange($design, $code, !initial);
-    if (!initial) window.location.hash = "#code";
-}
-
 $(document).ready(function () {
     document.querySelector('.banner-video').playbackRate = 1;
     filterSelection("featured");
     createFilter();
-    if (window.location.hash === "#design") {
-        $('.banner').css("background-color", "black");
-    }
     // landing page
     const $landingAnchor = $("#landing_anchor");
     const $landing = $("#landing");
     const $content = $(".content");
     const $zoomAnchor = $("#zoom_anchor");
 
-    const contentScale = .5;
     //nav bar
     const $navbar = $(".navbar");
     const $navAnchor = $("#nav_anchor");
@@ -110,32 +88,31 @@ $(document).ready(function () {
         zoomCenter = $zoomAnchor.offset();
         const imageHeight = $landingImg.width() * imgAspectRatio;
         topOffset = Math.min(0, ($window.height() - imageHeight) / 2);
-        // console.log(imageHeight, topOffset, zoomCenter);
-        // alert([$landingImg.height(), $landingImg.width() * imgAspectRatio, topOffset].join(" "));
-        // alert([topOffset, zoomCenter.left, zoomCenter.top, $window.width(), $window.height(), $landingImg.height()].join(" "));
-        // alert([$landingImg.height(), imgAspectRatio].join(" "));
         $landingImg.css("margin-top", topOffset);
         toggleLandingAndNavbar();
     }
 
-    let firstTimeScroll = window.location.hash === "";
+    let showZoom = (window.location.hash === "" && windowWidth > 500);
+
     $window.on('beforeunload', function () {
         // If during landing page animation, scroll to top when reloading so we don't confuse
         // the user.
         // Scroll to top before refreshing to make sure we start at the top after refresh.
         $window.unbind("scroll");
-        if (firstTimeScroll) $window.scrollTop(0);
+        if (showZoom) $window.scrollTop(0);
     });
-    if (!firstTimeScroll) {
+
+    if (!showZoom) {
         $content.removeClass('static');
         $landing.css("display", "none");
         $landing.addClass('hidden');
         $scrollDown.addClass('scroll-hidden');
         $slogan.removeClass('hidden');
+        $('.banner').css("background-color", "black");
     }
 
     $landing.on("transitionend", function () {
-        if (!firstTimeScroll || window.pageYOffset > maxYPos) {
+        if (!showZoom || window.pageYOffset > maxYPos) {
             $landing.css("display", "none");
             $('.banner').css("background-color", "black");
         }
@@ -143,7 +120,7 @@ $(document).ready(function () {
 
     function toggleLandingAndNavbar() {
         const yPosition = window.pageYOffset;
-        if (windowWidth >= 768 && firstTimeScroll) {
+        if (windowWidth >= 768 && showZoom) {
             if (yPosition <= maxYPos) {
                 // Visible.
                 $landing.css("display", "");
@@ -168,8 +145,8 @@ $(document).ready(function () {
             } else {
                 if (yPosition >= maxYPos2) {
                     $content.removeClass('static');
-                    if (firstTimeScroll) {
-                        firstTimeScroll = false;
+                    if (showZoom) {
+                        showZoom = false;
                         if (window.location.hash === "")
                             window.location.hash = "#design";
                         window.scrollTo(0, 0);
@@ -186,12 +163,6 @@ $(document).ready(function () {
 
     $window.scroll(toggleLandingAndNavbar);
     $window.resize(handleResize);
-
-    if (window.location.hash === "#code") {
-        turnOnCode(true);
-    } else {
-        turnOnDesign(true);
-    }
 
     $landingImg.on("load", handleResize);
     handleResize();
