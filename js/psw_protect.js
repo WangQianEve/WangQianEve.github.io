6d56188c11538a06b496b220e35921616749a7b1
Qian <eli_home@outlook.com>
2021-07-15 00:09:22 -0400

    google activation slides

--- js/psw_protect.js
+++ js/psw_protect.js
@@ -1,23 +1,24 @@
 // Password protection
 let projectName = "";
 let projectUrl = {
-  'activation': '35MnYANT.html',
-  'ring': 'XRgb9bwf.html',
+  'activation': 'https://docs.google.com/presentation/d/1MHx7nsu9_8CJW1cUffUehnOePQKtgVtWt9Zuezn5YEY/edit?usp=sharing',
+  'ring': './projects/XRgb9bwf.html',
 };
 
 function openPopup(project) {
-  document.getElementById("popup").open = true;
+  document.getElementById("popup").style.display = "block";
   projectName = project;
 }
 
 function closePopup() {
-  document.getElementById("popup").open = false;
+  document.getElementById("popup").style.display = "none";
 }
 
 function openProject() {
   $('#wrongpsw').removeClass('show');
   if ($('#psw').val() === 'watermelon') {
-    window.location.href = './projects/' + projectUrl[projectName];
+    window.open(projectUrl[projectName], '_blank');
+    // window.location.href = projectUrl[projectName];
   } else {
     $('#wrongpsw').addClass('show');
   }
